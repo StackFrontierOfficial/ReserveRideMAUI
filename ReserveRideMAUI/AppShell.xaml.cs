a8c1fca5836d33c46c4834e9e1f42b721defd8e5
code@stackfrontier.com <code@stackfrontier.com>
2024-03-15 00:53:13 +0530

    Initial Push

---
+++ ReserveRideMAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ReserveRideMAUI
+{
+	public partial class AppShell : Shell
+	{
+		public AppShell()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ ReserveRideMAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ReserveRideMAUI
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
